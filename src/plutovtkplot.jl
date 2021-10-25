128799b4efa9901edeb549b3a431206360ba63dd
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-10-25 10:35:44 +0200

    remove plane perturbation
    * contradicted to the assumption of invisibility of planes at min/max coordinate values

--- src/plutovtkplot.jl
+++ src/plutovtkplot.jl
@@ -295,18 +295,11 @@ function tetcontour!(p::PlutoVTKPlot, pts, tets,func; kwargs...)
     @views for idim=1:3
         xyzmin[idim]=minimum(pts[idim,:])
         xyzmax[idim]=maximum(pts[idim,:])
-    end
-    extent=maximum(xyzmax-xyzmin)
+    end 
 
-    # perturb planes to not to align with rect grid lines
-    xplanes=args[:xplanes].+(rand()-0.5)*1.0e-10*extent  
-    yplanes=args[:yplanes].+(rand()-0.5)*1.0e-10*extent  
-    zplanes=args[:zplanes].+(rand()-0.5)*1.0e-10*extent   
-
-
-
-  
-    
+    xplanes=args[:xplanes] 
+    yplanes=args[:yplanes] 
+    zplanes=args[:zplanes]  
         
     cpts0,faces0,values=GridVisualize.marching_tetrahedra(pts,tets,func,
                                                           primepoints=hcat(xyzmin,xyzmax),
